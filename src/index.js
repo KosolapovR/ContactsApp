825f0f2d8c27ee9e857f97ada09d09892ea0d5f9
Kosolapov Roman <Kosolapov-r@bk.ru>
2020-03-18 23:33:48 +0300

    add moment.js locale_ru

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import App from './App';
 import configureStore from "./app/state/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import {ConfigProvider} from "antd";
+import ruRU from "antd/es/locale/ru_RU";
+
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <ConfigProvider locale={ruRU}>
+                <App/>
+            </ConfigProvider>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root'));
